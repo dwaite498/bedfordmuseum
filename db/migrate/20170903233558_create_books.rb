c94b92d85046c816efda04b9e09f3bacb6425ca9
David Waite <dwaite498@gmail.com>
2017-09-14 18:48:08 +0000

    remove category string and add category_id integer to book model

--- db/migrate/20170903233558_create_books.rb
+++ db/migrate/20170903233558_create_books.rb
@@ -4,7 +4,6 @@ class CreateBooks < ActiveRecord::Migration
       t.string :title
       t.text :description
       t.string :author
-      t.string :category
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908173414) do
+ActiveRecord::Schema.define(version: 20170914184545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,13 +20,13 @@ ActiveRecord::Schema.define(version: 20170908173414) do
     t.string   "title"
     t.text     "description"
     t.string   "author"
-    t.string   "category"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "image_file_name"
     t.string   "price"
     t.string   "shipping"
     t.string   "paypal_link"
+    t.integer  "category_id"
   end
 
 end
