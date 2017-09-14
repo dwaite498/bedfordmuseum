08b5d627b2e0147ee4b071504dfbbc490105d29e
David Waite <dwaite498@gmail.com>
2017-09-14 19:17:54 +0000

    finished book filter and categories

--- app/controllers/bookstore_controller.rb
+++ app/controllers/bookstore_controller.rb
@@ -1,7 +1,12 @@
 class BookstoreController < ApplicationController
     
     def index
-        @books = Book.all
+        if params[:category].blank?
+            @books = Book.all.order("created_at DESC")
+        else
+            @category_id = Category.find_by(name: params[:category]).id
+            @books = Book.where(:category_id => @category_id).order("title DESC")
+        end
     end
     
     def show
