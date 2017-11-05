ed1db21d30dbaedd6627e6a82caef469df3a2e88
David Waite <dwaite498@gmail.com>
2017-11-05 20:49:47 +0000

    first two staff bios

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/membership', to: "index#membership"
   get '/research', to: "index#research"
   get '/schedule', to: "index#schedule"
+  get '/about', to: "index#about"
   
   resources :books
   
