780c37aa5d69985147919d79d1e5a7ddb39fc7b3
David Waite <dwaite498@gmail.com>
2017-12-18 02:48:10 +0000

    finish create and new actions in article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,25 @@ class ArticlesController < ApplicationController
         @articles = Article.all.order("created_at DESC")
     end
     
-    def  create
+    def new
+        @article = Article.new
     end
     
-    def new
+    def  create
+        @article = Article.new(article_params)
+        
+        if @article.save
+            redirect_to articles_path
+        else
+           render 'new'
+           flash[:alert] = "Error creating Article, please try again."
+        end
     end
+    
+private
+    
+    def article_params
+       params.require(:article).permit(:title, :body, :main_page, :main_page_index, :expiration_date) 
+    end
+
 end
