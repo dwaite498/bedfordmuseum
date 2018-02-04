cda9a0b52e750611cec4a5796c43617ee235183f
David Waite <dwaite498@gmail.com>
2018-02-04 18:44:38 +0000

    finish bookstore upload

--- db/booklist.rb
+++ db/booklist.rb
@@ -1,90 +1,16 @@
-book = Book.create!(
-   :title => "Liberty Falling",
-   :description => "Liberty Falling is a fictional, but accurate account of a true mountain family of the Civil War era surviving on the western slopes of Sharptop Mountain. A tragic accident leaves the father unable to provide for the family, forcing his three young sons to assume all household responsibilities. When the Union Army pours over the Peaks of Otter on their campaign to take Lynchburg, the boys become separated from their home. The two eldest set out on a trek for help from the Confederate Army rumored to be in Lynchburg. Their journey carries them through the many sights of the county and city, and ultimately leads them to a timely meeting with the army's commanders. Being granted return passage home, the brothers get caught up in a brutal cavalry skirmish as General Hunter's army approaches. The tragedies and unforeseen revelations that follow cause their trip to come full circle through the devastated town of Liberty and back to the concerned family and community that so anxiously been waiting",
-   :author => "G.L. McIver",
-   :category_id => 2,
-   :image_file_name => "classof1940.jpg",
-   :price => 14.95,
-   :shipping => 6.00,
-   :paypal_link => "<form target=""paypal"" action=""https://www.paypal.com/cgi-bin/webscr"" method=""post"">
-<input type=""hidden"" name=""cmd"" value=""_s-xclick"">
-<input type=""hidden"" name=""hosted_button_id"" value=""GCHKD6D8T5722"">
-<input type=""image"" src=""https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif"" border=""0"" name=""submit"" alt=""PayPal - The safer, easier way to pay online!"">
-<img alt="""" border=""0"" src=""https://www.paypalobjects.com/en_US/i/scr/pixel.gif"" width=""1"" height=""1"">
-</form>
-"
+require 'csv'
+
+csv_path = File.join(Rails.root, "db", "booklist.csv")
+CSV.foreach(csv_path, :headers => true) do |row| 
+   book = Book.create!(
+      :title => row["Title"],
+      :description => row["Description"],
+      :author => row["Author"],
+      :category_id => row["Category ID"].to_i,
+      :image_file_name => "classof1940.jpg",
+      :price => row["Price"],
+      :shipping => row["Shipping"],
+      :paypal_link => row["Paypal Link"]
    )
-book.save
-
-book = Book.create!(
-   :title => "The Bedford Boys",
-   :description => "June 6, 1944: Nineteen boys from rural Bedford, Virginia, died in the first bloody minutes of D-Day. They were part of Company A of the 116th Regiment of the 29th Division, and among the first wave of American soldiers to hit the beaches at Normandy. Later in the campaign, three more boys from this small Virginia community died of gunshot wounds.",
-   :author => "Alex Kershaw",
-   :category_id => 2,
-   :image_file_name => "classof1940.jpg",
-   :price => 16.95,
-   :shipping => 5,
-   :paypal_link => "<form target=""paypal"" action=""https://www.paypal.com/cgi-bin/webscr"" method=""post"">
-<input type=""hidden"" name=""cmd"" value=""_s-xclick"">
-<input type=""hidden"" name=""hosted_button_id"" value=""F9QSBWSY8LNPW"">
-<input type=""image"" src=""https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif"" border=""0"" name=""submit"" alt=""PayPal - The safer, easier way to pay online!"">
-<img alt="""" border=""0"" src=""https://www.paypalobjects.com/en_US/i/scr/pixel.gif"" width=""1"" height=""1"">
-</form>
-"
-)
-book.save
-
-book = Book.create!(
-   :title => "Remnants of War",
-   :description => "Civil War Records Bedford County, Virginia",
-   :author => "Ann Chilton",
-   :category_id => 2,
-   :image_file_name => "classof1940.jpg",
-   :price => 22,
-   :shipping => 6,
-   :paypal_link => "<form target=""paypal"" action=""https://www.paypal.com/cgi-bin/webscr"" method=""post"">
-<input type=""hidden"" name=""cmd"" value=""_s-xclick"">
-<input type=""hidden"" name=""hosted_button_id"" value=""DED7FEVJS7NBS"">
-<input type=""image"" src=""https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif"" border=""0"" name=""submit"" alt=""PayPal - The safer, easier way to pay online!"">
-<img alt="""" border=""0"" src=""https://www.paypalobjects.com/en_US/i/scr/pixel.gif"" width=""1"" height=""1"">
-</form>
-"
-)
-book.save
-
-book = Book.create!(
-   :title => "The Government Girls of WWII",
-   :description => "The Government Girls of WWII, tells story of an eighteen-year-old girl from Bedford working in Washington, D.C. The book also provides a glimpse of what women working in  Washington, D.C. did during the Second World War.",
-   :author => "Margaret Krantz Parks",
-   :category_id => 2,
-   :image_file_name => "classof1940.jpg",
-   :price => 10,
-   :shipping => 2.50,
-   :paypal_link => "<form target=""paypal"" action=""https://www.paypal.com/cgi-bin/webscr"" method=""post"">
-<input type=""hidden"" name=""cmd"" value=""_s-xclick"">
-<input type=""hidden"" name=""hosted_button_id"" value=""XQUDK4NB6HKW4"">
-<input type=""image"" src=""https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif"" border=""0"" name=""submit"" alt=""PayPal - The safer, easier way to pay online!"">
-<img alt="""" border=""0"" src=""https://www.paypalobjects.com/en_US/i/scr/pixel.gif"" width=""1"" height=""1"">
-</form>
-"
-)
-book.save
-
-
-book = Book.create!(
-   :title => "A Girls Life in Virginia Before the War",
-   :description => "A true story of a young girls life lived in Virginia Bedford during the Civil War. Excerpts from the diary of Letitia B. Burwell to her nieces were complied and put together to give an accurate depiction of A Girl's Life of Virginia Before the War",   
-   :author => "Letitia B. Burwell",
-   :category_id => 2,
-   :image_file_name => "classof1940.jpg",
-   :price => 25,
-   :shipping => 6.00,
-   :paypal_link => "<form target=""paypal"" action=""https://www.paypal.com/cgi-bin/webscr"" method=""post"">
-<input type=""hidden"" name=""cmd"" value=""_s-xclick"">
-<input type=""hidden"" name=""hosted_button_id"" value=""RVWR6DBAZ83K8"">
-<input type=""image"" src=""https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif"" border=""0"" name=""submit"" alt=""PayPal - The safer, easier way to pay online!"">
-<img alt="""" border=""0"" src=""https://www.paypalobjects.com/en_US/i/scr/pixel.gif"" width=""1"" height=""1"">
-</form>
-"
-)
-book.save
+   book.save
+end
