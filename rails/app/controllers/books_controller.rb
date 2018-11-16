77f76f9be8a9ac53254ddc55f48ca77cfe8f7e2a
david <dwaite498@gmail.com>
2018-11-16 01:40:24 +0000

    Start building books controller with tests

--- rails/app/controllers/books_controller.rb
+++ rails/app/controllers/books_controller.rb
@@ -1,2 +1,20 @@
 class BooksController < ApplicationController
+    def index
+        @books = Book.all
+    end
+
+    def create
+        @book = Book.new(book_params)
+        if @book.save
+            redirect_to books_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permit(:title, :description, :author, :price, :shipping, :paypal_link)
+    end
 end
--- rails/test/controllers/books_controller_test.rb
+++ rails/test/controllers/books_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Should get index' do
+    get books_url
+
+    assert_equal 'index', @controller.action_name
+    assert_response :success
+    assert_select '.col-md-3', Book.all.count
+  end
+
+  test 'Should create' do
+    initial_count = Book.all.count
+
+    post books_url(
+      book: {
+        title: 'title',
+        description: 'description',
+        author: 'author',
+        price: '1.2',
+        shipping: '3.4',
+        paypal_link: 'paypal_link'})
+
+    assert_redirected_to books_url
+    assert_equal Book.last.title, 'title'
+    assert_equal Book.all.count, initial_count + 1
+  end
 end
