7c1bc0216194388f1559c26aba86e5a09586804e
david <dwaite498@gmail.com>
2018-12-04 19:44:25 +0000

    move static pages to new project, implement boostrap in new prject

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root 'index#index'
+  
+  get '/directions', to: 'index#directions'
+  get '/membership', to: 'index#membership'
+  get '/research', to: 'index#research'
+  get '/schedule', to: 'index#schedule'
+  get '/about', to: 'index#about'
+  
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
