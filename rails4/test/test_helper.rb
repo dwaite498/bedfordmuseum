3772d6baa1d8a4a4e38f0849cd579d271e08b5ac
david <dwaite498@gmail.com>
2018-12-05 01:35:24 +0000

    make update test

--- rails4/test/test_helper.rb
+++ rails4/test/test_helper.rb
@@ -8,12 +8,11 @@ class ActiveSupport::TestCase
 
   def new_book
     post books_url(
-      book: {
-        title: 'title',
-        description: 'description',
-        author: 'author',
-        price: '1.2',
-        shipping: '3.4',
-        paypal_link: 'paypal_link'})
+      title: 'title',
+      description: 'description',
+      author: 'author',
+      price: '1.2',
+      shipping: '3.4',
+      paypal_link: 'paypal_link')
   end
 end
