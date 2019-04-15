e4f6bdb3920721d8b9b8e80d5500b022505557b8
david <dwaite498@gmail.com>
2019-04-15 14:38:13 +0000

    add image validation to images

--- rails/app/models/book.rb
+++ rails/app/models/book.rb
@@ -1,3 +1,14 @@
 class Book < ApplicationRecord
   has_one_attached :image
+  
+  validate :image_type
+  
+  def image_type
+    if image.attached? == false
+      errors.add(:image, "is missing!")
+    end
+    if !image.content_type.in?(%('image/jpeg image/png'))
+      errors.add(:image, "needs to be a jpeg or png!")
+    end
+  end
 end
