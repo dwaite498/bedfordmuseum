dfb941ba56640c44d27726d27898bef1abab5cd1
david <dwaite498@gmail.com>
2019-10-08 15:20:15 +0000

    update button layouts and styling for book editting

--- rails/app/models/book.rb
+++ rails/app/models/book.rb
@@ -1,15 +1,16 @@
 class Book < ApplicationRecord
   
   has_one_attached :image
-  
-  validate :image_type
-  
+
+validate :image_type
+
+private
+
   def image_type
     if image.attached? == false
       errors.add(:image, "is missing!")
-    end
-    if !image.content_type.in?(%('image/jpeg image/png'))
-      errors.add(:image, "needs to be a jpeg or png!")
+    elsif !image.content_type.in?(%('image/jpg image/png'))
+      errors.add(:image, "needs to be a jpg or png!")
     end
   end
 end
