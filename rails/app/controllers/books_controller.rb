c1add4f15c21abfd367fdc5617ea5c8d57f42418
david <dwaite498@gmail.com>
2020-03-07 15:42:56 +0000

    adjust memeber login to point to login page on old interface, remove category filter if category is empty

--- rails/app/controllers/books_controller.rb
+++ rails/app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-    before_action :authenticate_admin!, except: [:show, :index]
+    # before_action :authenticate_admin!, except: [:show, :index]
     
     def index
         if params[:category].blank?
